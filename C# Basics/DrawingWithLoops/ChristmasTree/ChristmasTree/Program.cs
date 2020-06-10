f742680c34c315295dae11cc52b120a7464549e0
Fennecky <faerthurimben@gmail.com>
2020-06-10 23:14:07 +0300

    Included new loop drawing excersises

---
+++ C# Basics/DrawingWithLoops/ChristmasTree/ChristmasTree/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ChristmasTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var spaces = n;
+            for (int i = 0; i < spaces + 2; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("|");
+            for (int row = 1; row <= n; row++)
+            {
+                for (int i = 0; i < spaces; i++)
+                {
+                    Console.Write(" ");
+                }
+                for (int i = 0; i < row; i++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write(" | ");
+                for (int i = 0; i < row; i++)
+                {
+                    Console.Write("*");
+                }
+                spaces--;
+                Console.WriteLine();
+
+            }
+
+        }
+    }
+}
---
+++ C# Basics/DrawingWithLoops/RombDrawing/RombDrawing/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RombDrawing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var spaces = n - 1;
+            for (int row = 1; row <= n; row++)
+            {
+                for (int i = 0; i < spaces; i++)
+                {
+                    Console.Write(" ");
+                }
+                for (int i = 0; i < row; i++)
+                {
+                    Console.Write("* ");
+                }
+                spaces--;
+                Console.WriteLine();
+            }
+
+            var reverseSpaces = 1;
+            for (int row = n-1; row > 0; row--)
+            {
+                for (int i = 0; i < reverseSpaces; i++)
+                {
+                    Console.Write(" ");
+                }
+                for (int i = 0; i < row; i++)
+                {
+                    Console.Write("* ");
+                }
+                reverseSpaces++;
+                Console.WriteLine();
+            }
+        }
+    }
+}

d52ddd2f74c4da7db170adc8faf0ec32b31ed254
Fennecky <faerthurimben@gmail.com>
2020-08-20 13:44:39 +0300

    Expanded old projects and created new ones

---
+++ C# Basics/Advanced-Loops/Even_Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    int n = int.Parse(Console.ReadLine());
+                    if (n % 2 == 0)
+                    {
+                        Console.WriteLine("The number {0} is even", n);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The number {0} is not even", n);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("The number is invalid!");
+                }
+            }
+        }
+    }
+}
--- C# Basics/Advanced-Loops/Prime_Numbers/Program.cs
+++ C# Basics/Advanced-Loops/Prime_Numbers/Program.cs
@@ -6,7 +6,26 @@ namespace Prime_Numbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please enter a number: ");
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Checking your number...");
+            bool prime = true;
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+            if (prime)
+            {
+                Console.WriteLine("The number {0} is prime", n);
+            }
+            else
+            {
+                Console.WriteLine("The number {0} is not prime", n);
+            }
         }
     }
 }

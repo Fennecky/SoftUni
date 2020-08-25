cd9951acc9bf5b837dca35ecd76254fdae591c44
Fennecky <faerthurimben@gmail.com>
2020-08-25 14:16:36 +0300

    Included new project and tasks

---
+++ Programming_For_Beginners/Massives (Arrays)/Day_Of_Week/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Day_Of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a day!: ");
+            string[] days = { "Monday", "Thusday", "Wednsday", "Thursday", "Friday", "Satruday", "Sunday" };
+            int day = int.Parse(Console.ReadLine());
+            
+            if (day >= 0 && day <= 7)
+            {
+                Console.Write("The day is: ");
+                Console.Write(days[day - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
---
+++ Programming_For_Beginners/Massives (Arrays)/Sieve_of_Eratosthenes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Sieve_of_Eratosthenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool[] primes = new bool[n + 1];
+            for (int i = 2; i <= n; i++)
+            {
+                primes[i] = true;
+            }
+            for (int p = 2; p <= n; p++)
+            {
+                if (primes[p]) { FillPrimes(primes, p); }
+
+            }
+            
+        }
+
+        static void FillPrimes(bool[] primes, int stat)
+        {
+            for (int i = 2 * stat; i <= primes.Length; i += stat)
+            {
+                primes[i] = false;
+                Console.WriteLine($"Not Prime {i}");
+            }
+
+        }
+    }
+}

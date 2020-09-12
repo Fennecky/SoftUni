26dc4b1c782fbff7b773cdf784605a1543d5cd6b
Fennecky <faerthurimben@gmail.com>
2020-09-12 15:53:24 -0700

    Finished lecture section and finished task

---
+++ Programming_For_Beginners/Lists and Matrices/Count_Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split(' ').Select(long.Parse).ToList();
+            nums.Sort();
+            var counter = new long[nums.Max() + 1];
+            foreach (var num in nums)
+            {
+                counter[num]++;
+            }
+            for (int i = 0; i < counter.Length; i++)
+            {
+                if (counter[i] > 0)
+                {
+                    Console.WriteLine($"{i} => {counter[i]}");
+                }
+            }
+
+        }
+    }
+}

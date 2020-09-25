735266d15f4f24499944439847d21c26a2c498b9
Fennecky <faerthurimben@gmail.com>
2020-09-25 00:08:37 -0700

    Finished some tasks. Started dictionary

---
+++ Programming_For_Beginners/Strings, Dictionaries, Lambda and LINQ/Change_Forbidden_Substrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Change_Forbidden_Substrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = Console.ReadLine();
+            string[] words = Console.ReadLine().Split(' ');
+
+            foreach (var word in words)
+            {
+                str = str.Replace(word, new string('*', word.Length));
+            }
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ Programming_For_Beginners/Strings, Dictionaries, Lambda and LINQ/Count_Real_Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split(' ').Select(double.Parse).ToList();
+            var count = new SortedDictionary<double,int>();
+
+            foreach (var num in nums)
+            {
+                if (count.ContainsKey(num))
+                {
+                    count[num]++;
+                }
+                else
+                {
+                    count[num] = 1;
+                }
+            }
+
+            foreach (var pair in count)
+            {
+                Console.WriteLine("{0} -> {1}", pair.Key, pair.Value);
+            }
+
+        }
+    }
+}
